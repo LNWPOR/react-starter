9b10a7c844273b9fe8de1120b16f0985c01e348a
LNWPOR <tclnwpor@gmail.com>
2016-10-23 21:58:09 +0700

    remove json-server, and add some my api call at Home

--- ui/common/components/Home.js
+++ ui/common/components/Home.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 import styles from './Home.scss'
+import fetch from 'isomorphic-fetch'
+
 
 class Home extends Component {
-  render() {
-    return (
-      <h1>Home</h1>
-    )
-  }
+	state = {
+	    lists: []
+	}
+
+
+	componentDidMount() {
+    	fetch('http://127.0.0.1:8000/api/lists')
+      		.then((response) => response.json())
+      		.then((lists) => this.setState({ lists }))
+  	}
+
+	render() {
+	    return (
+	    	<div>
+	    		<h1>Home</h1>
+				{
+		            this.state.lists.map((list) => (
+		            	<div key={list._id}>
+		            		<h2>{list._id}</h2>
+			            	<h2>{list.description}</h2>
+			            	<h2>{list.ownerID}</h2> 
+		            	</div>
+		            ))
+		        }
+	    	</div>
+	    )
+	}
 }
 
 export default Home
