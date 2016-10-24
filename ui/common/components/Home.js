7781106fa1deda0ff1ef5948812b06bb154afee4
LNWPOR <tclnwpor@gmail.com>
2016-10-25 00:02:25 +0700

    comment call api

--- ui/common/components/Home.js
+++ ui/common/components/Home.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react'
 import styles from './Home.scss'
-import fetch from 'isomorphic-fetch'
+// import fetch from 'isomorphic-fetch'
 
 
 class Home extends Component {
-	state = {
-	    lists: []
-	}
+	// state = {
+	//     lists: []
+	// }
 
 
-	componentDidMount() {
-    	fetch('http://127.0.0.1:8000/api/lists')
-      		.then((response) => response.json())
-      		.then((lists) => this.setState({ lists }))
-  	}
+	// componentDidMount() {
+ //    	fetch('http://127.0.0.1:8000/api/lists')
+ //      		.then((response) => response.json())
+ //      		.then((lists) => this.setState({ lists }))
+ //  	}
 
 	render() {
 	    return (
 	    	<div>
 	    		<h1>Home</h1>
 				{
-		            this.state.lists.map((list) => (
-		            	<div key={list._id}>
-		            		<h2>{list._id}</h2>
-			            	<h2>{list.description}</h2>
-			            	<h2>{list.ownerID}</h2> 
-		            	</div>
-		            ))
+		            // this.state.lists.map((list) => (
+		            // 	<div key={list._id}>
+		            // 		<h2>{list._id}</h2>
+			           //  	<h2>{list.description}</h2>
+			           //  	<h2>{list.ownerID}</h2> 
+		            // 	</div>
+		            // ))
 		        }
 	    	</div>
 	    )
--- ui/config.js
+++ ui/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: '127.0.0.1',
-  apiPort: 5000,
+  apiPort: 8000,
   serverPort: 8080,
   clientPort: 8081
 }
